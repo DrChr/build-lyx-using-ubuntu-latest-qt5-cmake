9b83efbbeae98ab3f932e9996a842aea75faf395
ci <ci@lyx-linux3.cs1cloud.internal>
2017-01-19 22:27:49 +0100

    Include 'make package' and 'make package_source' to build command

--- build_lyx.sh
+++ build_lyx.sh
@@ -1,7 +1,34 @@
-#!/bin/sh
+#!/bin/bash
+
+# Change directory to folder with cloned LyX repository
 cd "$1"
+
+function failed() {
+    printf "Command '%s' failed, exiting\n" "$*"
+    exit -1
+}
+
+function do_or_fail() {
+    printf "# Executing: %s\n" "$*"
+    "$@" || failed "$@"
+}
 
 # Add a directory out-of-source for building
 mkdir -p build
 cd build
-cmake -DLYX_USE_QT=QT5 .. && make
+CMAKE_OPTS=( 
+    -DLYX_USE_QT=QT5
+    -DLYX_CPACK=ON
+    -DCPACK_BINARY_DEB:BOOL=ON
+    -DCPACK_SOURCE_TGZ:BOOL=ON
+    -DLYX_NLS=ON
+    -DLYX_EXTERNAL_Z=ON
+    -DLYX_EXTERNAL_ICONV=ON
+    -DLYX_EXTERNAL_BOOST=OFF
+)
+
+# CMAKE_OPTS=( -DLYX_USE_QT=QT5  -DLYX_CPACK=ON  -DCPACK_SOURCE_TGZ:BOOL=ON )
+
+do_or_fail cmake "${CMAKE_OPTS[@]}" ..
+do_or_fail make package
+do_or_fail make package_source
